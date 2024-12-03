8bb0680dadd22fb90fa440a4a571b4512c3ed1d3
PMediaJairo <jairo@profitablemedia.com>
2024-12-03 18:44:18 -0500

    Created the models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['customer', 'product_id','total', 'status'];
 }
