d8783bef607d61e6d30bd2a3e34987dfc917f16a
PMediaJairo <jairo@profitablemedia.com>
2024-12-03 22:17:10 -0500

    Products Api Logic

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,4 +11,29 @@ class ProductController extends Controller
     {
         return response()->json(Product::all());
     }
+
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return response()->json($product, 201);
+    } 
+    
+    public function show(Product $product)
+    {
+        return response()->json($product);
+    }
+    
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+        return response()->json($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+        return response()->json(null, 204);
+    }
+
+
 }
