4d7fa8d4866b535b30b13c3e38c51152dd8464d5
PMediaJairo <jairo@profitablemedia.com>
2024-12-04 15:27:27 -0500

    Order control enhancements

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,10 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Order;
 
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
-{
-    //
+{    
+    public function index()
+    {
+        return response()->json(Order::all());
+    }
+
+    public function store(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'product_id' => 'required|exists:products,id',            
+        ]);
+
+
+        $order = Order::create($request->all());
+        return response()->json($order, 201);
+    }    
+    public function show(Order $order)
+    {
+        return response()->json($order);
+    }    
+
+    public function update(Request $request, Order $order)
+    {
+        $order->update($request->all());
+        return response()->json($order);
+    }
+    public function destroy(Order $order)
+    {
+        $order->delete();
+        return response()->json(null, 204);
+    }    
+
 }
